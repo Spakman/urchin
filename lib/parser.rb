1a88a7a402ac0735005d1466c9e01180d1fe6d73
Mark Somerville <mark@scottishclimbs.com>
2010-10-20 17:05:36 +0100

    Parser TODO: list of example commands lines

--- lib/parser.rb
+++ lib/parser.rb
@@ -6,6 +6,57 @@ require "#{File.dirname(__FILE__)}/command"
 
 module Urchin
   # Really dumb command parser for now.
+  #
+  # TODO: implement a proper parser.
+  #
+  #   Here are some cases that it should be able to handle:
+  #
+  #   Pipelines:
+  #
+  #   * ls -la | head | tail
+  #
+  #   Seperate jobs:
+  #
+  #   * uptime ; sleep 1 ; top
+  #   * sleep 60 & echo 123
+  #
+  #   Quoting and escaping:
+  #
+  #   * grep -r "this is a quoted string" .
+  #   * grep -r 'this is a quoted string' .
+  #   * grep -r this\ is\ an\ escaped\ string .
+  #
+  #   Redirects:
+  #
+  #   * ls > output
+  #   * ls >> output
+  #   * patch -p0 < code.patch
+  #   * ls 2>&1
+  #   * patch -p0 2>&1 > output < code.patch
+  #
+  #   Command/tilde/alias expansion:
+  #
+  #   * ls -lh `cat thepath` | head -2
+  #   * ls ~/.config
+  #
+  #   Environment variables:
+  #
+  #   * VAR=hello echo $VAR; echo $VAR
+  #
+  #   Inline Ruby code:
+  #
+  #   * ls | ~@ p STDIN.gsub(/^*\..*/, "") @~ | tail
+  #
+  #   Simple calculations:
+  #
+  #   * (12 * 56) - 33
+  #
+  #   Globbing/brace expansion:
+  #
+  #   * ls {img,image}.{png,jpg}
+  #   * ls *.{png,jpg}
+  #   * ls **/*.{png,jpg}
+  #   * ls 00[123].{png,jpg}
   class Parser
     def initialize(shell)
       @shell = shell
