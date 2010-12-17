3775a2120b219418180818ba53f8cea115b4d418
Mark Somerville <mark@scottishclimbs.com>
2010-12-17 17:03:57 +0000

    Shell: added method comments

--- lib/shell.rb
+++ lib/shell.rb
@@ -5,6 +5,7 @@
 module Urchin
   class Shell
     attr_reader :job_table, :terminal_modes, :history
+
     @@aliases = {}
 
     def initialize
@@ -15,14 +16,21 @@ module Urchin
       @history = History.new
     end
 
-    def self.alias(hash)
-      @@aliases.merge! hash
+    # Starts the command line processing loop.
+    def run
+      setup_terminal_and_signals
+      begin
+        while input = Readline.readline(prompt)
+          @history.append input
+          parse_and_run input
+        end
+      rescue Interrupt
+        puts "\n^C"
+        retry
+      end
     end
 
-    def aliases
-      @@aliases
-    end
-
+    # Parse the command string and run any jobs within it in turn.
     def parse_and_run(command_string)
       @parser.jobs_from(command_string).each do |job|
         begin
@@ -34,22 +42,6 @@ module Urchin
         rescue Interrupt
           puts ""
         end
-      end
-    end
-
-    # Starts the command line processing loop.
-    #
-    # TODO: nicer history management.
-    def run
-      setup_terminal_and_signals
-      begin
-        while input = Readline.readline(prompt)
-          @history.append input
-          parse_and_run input
-        end
-      rescue Interrupt
-        puts "\n^C"
-        retry
       end
     end
 
@@ -79,33 +71,6 @@ module Urchin
       Termios.tcsetpgrp(STDIN, Process.getpgrp)
     end
 
-    # Defines the prompt method.
-    def self.prompt(&block)
-      define_method :prompt do
-        block.call
-      end
-    end
-
-    if RUBY_PLATFORM =~ /linux/
-      # TODO: consider Bash-like escaping (\[\]).
-      def prompt
-        "\001\e[0;36m\002(\001\e[1;32m\002#{Dir.getwd}\001\e[0;36m\002)\001\033[0m\002% "
-      end
-    elsif RUBY_PLATFORM =~ /darwin/
-      # OS X doesn't seem to like (or need) the \001\002 escaping, at least on
-      # the box I have SSH access to.
-
-      def prompt
-        "\e[0;36m(\e[1;32m#{Dir.getwd}\e[0;36m)\033[0m% "
-      end
-    else
-      # Other platforms are untested.
-
-      def prompt
-        "(#{Dir.getwd}) % "
-      end
-    end
-
     # Foreground child processes can also be caught by the
     # Job itself, but we need to add this here to ensure
     # there are no zombies.
@@ -114,6 +79,51 @@ module Urchin
         @job_table.jobs.each do |job|
           job.reap_children Process::WNOHANG
         end
+      end
+    end
+
+    # Adds hash to the list of aliases.
+    #
+    # These are set in URCHIN_RB, like so:
+    #
+    # Shell.alias "ls" => "ls --color"
+    def self.alias(hash)
+      @@aliases.merge! hash
+    end
+
+    # Returns the hash of aliases.
+    def aliases
+      @@aliases
+    end
+
+    # Defines the prompt method.
+    def self.prompt(&block)
+      define_method :prompt do
+        block.call
+      end
+    end
+
+    if RUBY_PLATFORM =~ /linux/
+      # Returns the prompt for terminal display.
+      #
+      # TODO: consider Bash-like escaping (\[\]).
+      def prompt
+        "\001\e[0;36m\002(\001\e[1;32m\002#{Dir.getwd}\001\e[0;36m\002)\001\033[0m\002% "
+      end
+    elsif RUBY_PLATFORM =~ /darwin/
+      # OS X doesn't seem to like (or need) the \001\002 escaping, at least on
+      # the box I have SSH access to.
+
+      # Returns the prompt for terminal display.
+      def prompt
+        "\e[0;36m(\e[1;32m#{Dir.getwd}\e[0;36m)\033[0m% "
+      end
+    else
+      # Other platforms are untested.
+
+      # Returns the prompt for terminal display.
+      def prompt
+        "(#{Dir.getwd}) % "
       end
     end
   end
