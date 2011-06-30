728c0afa7c19960ee72355b2bd0e89059c9dde7e
Mark Somerville <mark@scottishclimbs.com>
2011-06-30 11:55:18 +0100

    Remove a warning when running the git completion tests
    Tested-by: Mark Somerville <mark@scottishclimbs.com> [F14, MRI 1.8.7/1.9.2]
    Signed-off-by: Mark Somerville <mark@scottishclimbs.com>

--- test/completion/git_test.rb
+++ test/completion/git_test.rb
@@ -3,13 +3,6 @@ require "#{File.dirname(__FILE__)}/../../completion/git"
 
 module Urchin
   module Completion
-
-    class Git
-      class << self
-        attr_reader :commands
-      end
-    end
-
     class GitTestCase < Test::Unit::TestCase
       def test_commands_with_empty_first_arg
         assert_equal Git.commands, Git.new.complete((Command.create("git", nil)), "")
