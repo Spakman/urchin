2f3a037a4d328af72690f4f5fe0c4273cf2e56fb
Mark Somerville <mark@fingers.today>
2023-01-21 12:43:59 +0000

    Call Builtin.make to allow for overriding
    This allows defining the now probably poorly named Builtins inside
    the urchin.rb config file such that they can return OSProcess instances.
    
    The first one I've implemented is one always all quite intricate
    arguments to SSH. In this case a simple alias will not do because the
    static/added arguments must come AFTER the chaning hostname arguemnt.
    
    Signed-off-by: Mark Somerville <mark@fingers.today>

--- lib/builtin.rb
+++ lib/builtin.rb
@@ -6,6 +6,13 @@ module Urchin
   class Builtin < Command
     public_class_method :new
 
+    # This can be overridden in order to return an OSProcess instance, for
+    # example. A tad unclean perhaps, but more straightforward for now than
+    # implementing a new "functions" feature for user defined stuff.
+    def self.build(job_table)
+      self.new(job_table)
+    end
+
     def initialize(job_table)
       @job_table = job_table
       super(self.class::EXECUTABLE, job_table)
@@ -38,7 +45,7 @@ module Urchin
     # Returns an instance of the Builtin class for a given executable or nil.
     def self.command_for(executable, job_table)
       if builtins[executable]
-        builtins[executable].new(job_table)
+        builtins[executable].build(job_table)
       end
     end
   end
